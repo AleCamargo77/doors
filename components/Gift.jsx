a02b66c607fedda433c97e85cdfd94ef13d41a98
Alessandro Camargo <acamargodias@terra.com.br>
2023-02-18 21:59:36 -0300

    feat: first commit

---
+++ components/Gift.jsx
@@ -0,0 +1,10 @@
+import styles from "../src/styles/gift.module.css";
+
+export default function Gift() {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+    </div>
+  );
+}
